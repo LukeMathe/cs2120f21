14e99ec5da844e27cce6a44f157cac58653ebd34
Luke Mathe <lkm6eka@virginia.edu>
2021-10-15 19:40:40 +0000

    class notes

---
+++ src/mywork/oct_13.lean
@@ -0,0 +1,13 @@
+/-
+Set is a colleciton of objects in some structure
+Types of sets:
+Empty
+Complete
+Union- combines 2 sets
+Intersection- set with shared elements of both sets
+Complement- elements not in one set
+Power- all of possible subsets in a given set
+        calculate 2 ^ (# elements) to find number of sets
+Subset- set made up of elements of another set
+Product- set of all possible pairs of elements
+-/
